909ca0096dd47fa4f6778637c6bc5b50b27a3abf
Sefffzxc <macosefrowinston@gmail.com>
2026-03-06 23:56:21 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// App.js
+// App.jsx
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate, useLocation, useNavigate } from 'react-router-dom';
 import './App.css';
